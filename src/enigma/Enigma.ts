284716c796af116425256d9a450e1fa40af7a8d0
Steven Kurniawan Chandra <steven.k.chandra@gmail.com>
2024-08-18 23:37:40 +0100

    new test cases:      test/enigmaTest.js:         Rotor: check its getter functions         EnigmaSetting
    added new feature:
         enigma/enigma.js:
            new class EnigmaSetting
            new class Enigma (only basic static variables)

--- src/enigma/Enigma.ts
+++ src/enigma/Enigma.ts
@@ -38,10 +38,19 @@ enum RotorType {
     VIII = "delapan",
 }
 
+/**
+ * Helper function that checks if a string is an alphabetical character or not
+ * @param c string to check
+ * @returns true iff c is an alphabetical character
+ */
+function isAlphabeticalChar(c: string): boolean {
+    return !(c.length !== 1 || !c.charAt(0).match(/[a-zA-Z]/))
+}
+
 export class Rotor {
-    private readonly rotorType: RotorType;
-    private readonly ringSetting: number;
-    private readonly intialSetting: number;
+    private readonly rotor: RotorType;
+    private readonly ring: string;
+    private readonly initial: string;
 
     /**
      * Consist of:
@@ -50,7 +59,6 @@ export class Rotor {
      */
     public static readonly RotorType = RotorType;
 
-    private static readonly codeforA: number = "A".charCodeAt(0);
     /**
      * Make a new Reflector Object
      * @param reflectorType is an enumerated RotorType from Rotor.RotorType
@@ -58,16 +66,90 @@ export class Rotor {
      * @param ringSetting takes an alphabetical character / string of length 1
      */
     constructor(rotorType: RotorType, ringSetting: string, initialSetting: string){
-        if (ringSetting.length !== 1 || !ringSetting.charAt(0).match(/[a-zA-Z]/)){
-            throw Error("ringSetting only takes a single alphabet character");
+        if (!isAlphabeticalChar(ringSetting)){
+            throw new Error("ringSetting only takes a single alphabet character");
         }
-        if (initialSetting.length !== 1 || !initialSetting.charAt(0).match(/[a-zA-Z]/)){
-            throw Error("initialSetting only takes a single alphabet character");
+        if (!isAlphabeticalChar(initialSetting)){
+            throw new Error("initialSetting only takes a single alphabet character");
         }
 
-        this.rotorType = rotorType;
-        this.ringSetting = ringSetting.toUpperCase().charCodeAt(0) - Rotor.codeforA;
-        this.intialSetting = initialSetting.toUpperCase().charCodeAt(0) - Rotor.codeforA;
+        this.rotor = rotorType;
+        this.ring = ringSetting.toUpperCase();
+        this.initial = initialSetting.toUpperCase();
     }
+
+    public get rotorType(): RotorType {
+        return this.rotor;
+    }
+
+    public get ringSetting(): string {
+        return this.ring;
+    }
+
+    public get intitialSetting(): string {
+        return this.initial;
+    }
+
+}
+
+export class EnigmaSetting {
+    private readonly reflector: Reflector;
+    private readonly rotor:  Array<Rotor>;
+    private readonly plugboard: Array<[string, string]>;
+
+    /**
+     * Make a new EnigmaSetting object
+     * @param reflector is a Reflector object
+     * @param rotor is an array of Rotors object, with length 3 or 4
+     * @param plugboard is an array of a single alphabetical character pairs, plugboard.length < 10
+     */
+    constructor(reflector: Reflector, rotor: Array<Rotor>, plugboard: Array<[string, string]>) {
+        if (!(rotor.length == 4 || rotor.length == 3)){
+            throw new Error("rotors.length must be 3 or 4");
+        }
+        this.checkPlugboard(plugboard);
+
+        this.reflector = reflector;
+        this.rotor = new Array<Rotor> ();
+        rotor.forEach(value => this.rotor.push(new Rotor(value.rotorType, value.ringSetting, value.intitialSetting)));
+        this.plugboard = new Array<[string, string]> ();
+        plugboard.forEach(value => this.plugboard.push([value[0], value[1]]));
+    }
+
+    private checkPlugboard(plugboard: Array<[string, string]>): void {
+        if (plugboard.length > 10){
+            throw new Error("plugboard.length must be between 0 and 10 (inclusive)");
+        }
+        let set: Set<string> = new Set<string> ();
+        for (let connection of plugboard){
+            if (!isAlphabeticalChar(connection[0]) || !isAlphabeticalChar(connection[1])){
+                throw new Error("plugboard element must be a single alphabet character");
+            }
+            if (set.has(connection[0]) || set.has(connection[1])){
+                throw new Error("plugboard cannot contain duplicate element");
+            }
+            set.add(connection[0]);
+            set.add(connection[1]);
+        }
+    }
+}
+
+export class Enigma {
+
+    private static reflectorWiring: Array<string> = new Array<string> (
+        "YRUHQSLDPXNGOKMIEBFZCWVJAT",
+        "FVPJIAOYEDRZXWGCTKUQSBNMHL",
+    )
+
+    private static rotorWiring: Array<string> = new Array<string> (
+        "EKMFLGDQVZNTOWYHXUSPAIBRCJ",
+        "AJDKSIRUXBLHWTMCQGZNPYFVOE",
+        "BDFHJLCPRTXVZNYEIWGAKMUSQO",
+        "ESOVPZJAYQUIRHXLNFTGKDCMWB",
+        "VZBRGITYUPSDNHLXAWMJQOFECK",
+        "JPGVOUMFYQBENHZRDKASXLICTW",
+        "NZJHGRCXMYSWBOUFAIVLPEKQDT",
+        "FKQHTLXOCBJSPDZRAMEWNIUYGV",
+    )
 
 }
--- src/test/EnigmaTest.ts
+++ src/test/EnigmaTest.ts
@@ -1,6 +1,6 @@
-import { Reflector, Rotor} from "../enigma/Enigma"
-
-const assert = require('node:assert/strict');
+import assert from 'node:assert/strict';
+import { Rotor, EnigmaSetting, Reflector} from "../enigma/Enigma";
+import exp from 'node:constants';
 
 describe("Rotor", function() {
     /**
@@ -13,31 +13,105 @@ describe("Rotor", function() {
      *          length 1, length not 1
      */
     it("accepts ringSetting alphabet length 1, " +
-        "initialSetting alphabet length 1", function() {
-
+        "initialSetting alphabet length 1, ", function() {
+        new Rotor(Rotor.RotorType.I, "a", "b");
+    });
+    it("getter functions return correct values", function() {
         const rotor = new Rotor(Rotor.RotorType.I, "a", "b");
+        assert.deepStrictEqual(rotor.rotorType, Rotor.RotorType.I, "getter function rotorType returns wrong value");
+        assert.deepStrictEqual(rotor.ringSetting, "A", "getter function ringSetting returns wrong value");
+        assert.deepStrictEqual(rotor.intitialSetting, "B", "getter function initialSetting returns wrong value");
     });
     it("does not accepts ringSetting non alphabet", function() {
-        const err: Error = new Error ("ringSetting only takes a single alphabet character");
+        const expectedError: Error = new Error ("ringSetting only takes a single alphabet character");
         assert.throws(function () {
-            const rotor = new Rotor(Rotor.RotorType.I, "1", "b");
-        })
+            new Rotor(Rotor.RotorType.I, "1", "b");
+        }, expectedError);
     });
     it("does not accepts ringSetting.length not 1", function() {
-        const err: Error = new Error ("ringSetting only takes a single alphabet character");
+        const expectedError: Error = new Error ("ringSetting only takes a single alphabet character");
         assert.throws(function () {
-            const rotor = new Rotor(Rotor.RotorType.I, "ab", "b");
-        })
+            new Rotor(Rotor.RotorType.I, "ab", "b");
+        }, expectedError);
     });
     it("does not accepts initialSetting non alphabet", function() {
+        const expectedError: Error = new Error("initialSetting only takes a single alphabet character");
         assert.throws(function () {
-            const rotor = new Rotor(Rotor.RotorType.I, "a", "2");
-        })
+            new Rotor(Rotor.RotorType.I, "a", "2");
+        }, expectedError);
     });
     it("does not accepts initialSetting.length not 1", function() {
-        const err: Error = new Error("initialSetting only takes a single alphabet character");
+        const expectedError: Error = new Error("initialSetting only takes a single alphabet character");
         assert.throws(function () {
-            const rotor = new Rotor(Rotor.RotorType.I, "a", "bc");
-        })
+            new Rotor(Rotor.RotorType.I, "a", "bc");
+        }, expectedError);
+    });
+});
+
+describe("EnigmaSetting", function() {
+    /**
+     *  Testing Strategy:
+     * 
+     *      partition on reflector:
+     *          
+     *      partition on rotor:
+     *          3 <= rotor.length <= 4, rotor.length < 3, rotor.length > 4
+     * 
+     *      partition on plugboard:
+     *          length = 0, 0 < length < 10, length = 10, length > 10
+     *          there exist a duplicate character, no duplicate character
+     *          there exist an element that is not alphabetical character, all elements are alphabetical character
+     */
+    
+    const reflector: Reflector = new Reflector(Reflector.ReflectorType.UKWC);
+    const rotors: Array<Rotor> = new Array <Rotor> (
+        new Rotor(Rotor.RotorType.I, "a", "b"),
+        new Rotor(Rotor.RotorType.IV, "c", "d"),
+        new Rotor(Rotor.RotorType.III, "x", "y"),
+        new Rotor(Rotor.RotorType.V, "i", "j"),
+        new Rotor(Rotor.RotorType.VIII, "m", "n"),
+    );
+    it("accept rotors.length of exactly 3, " +
+        "accepts plugboard.length of 0", function () {
+        new EnigmaSetting(reflector, rotors.slice(0, 3), []);
+    });
+    it("accept rotors.length of exactly 4, " +
+        "accepts 0 < plugboard.length < 10, " +
+        "all elements are alphabetical character, " +
+        "no duplicate character", function () {
+        new EnigmaSetting(reflector, rotors.slice(1, 5), [["a", "b"], ["c", "d"]]);
+    });
+    it("accepts plugboard.length = 10", function () {
+        new EnigmaSetting(reflector, rotors.slice(1, 4), [["a", "b"], ["c", "d"], ["e", "f"], ["g", "h"], ["i", "j"], ["k", "l"], ["m", "n"], ["o", "p"], ["q", "r"], ["s", "t"]]);
+    });
+    it("rejects rotor.length < 3 ", function() {
+        const expectedError: Error = new Error ("rotors.length must be 3 or 4");
+        assert.throws(function() {
+            new EnigmaSetting(reflector, rotors.slice(1, 3), []);
+        }, expectedError);
+    });
+    it("rejects rotor.length > 4 ", function() {
+        const expectedError: Error = new Error ("rotors.length must be 3 or 4");
+        assert.throws(function() {
+            new EnigmaSetting(reflector, rotors.slice(0, 5), [["a", "b"], ["c", "d"]]);
+        }, expectedError);
+    });
+    it("rejets plugboard.length > 10", function (){
+        const expectedError: Error = new Error ("plugboard.length must be between 0 and 10 (inclusive)");
+        assert.throws(function() {
+            new EnigmaSetting(reflector, rotors.slice(2, 5), [["a", "b"], ["c", "d"], ["e", "f"], ["g", "h"], ["i", "j"], ["k", "l"], ["m", "n"], ["o", "p"], ["q", "r"], ["s", "t"], ["u", "v"]]);
+        }, expectedError);
+    });
+    it("rejects if there exist an element that is not alphabetical character", function (){
+        const expectedError: Error = new Error ("plugboard element must be a single alphabet character");
+        assert.throws(function() {
+            new EnigmaSetting(reflector, rotors.slice(0, 3), [["a", "be"], ["d", "c"]]);
+        }, expectedError);
+    });
+    it("rejets plugboard with an duplicate element", function (){
+        const expectedError: Error = new Error ("plugboard cannot contain duplicate element");
+        assert.throws(function() {
+            new EnigmaSetting(reflector, rotors.slice(0, 3), [["a", "b"], ["b", "c"]]);
+        }, expectedError);
     });
 });
